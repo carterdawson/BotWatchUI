99a5971e10c2b3b0ddda970a915cfb67c6494ce5
Shaun Dawson <scdawson@gmail.com>
2020-04-27 11:54:17 -0700

    Flesh out login capability

---
+++ dist/js/config.js
@@ -0,0 +1,12 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_XMU3rtrtY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2dt9svrbbtr4usea7i3muco9pk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        baseUrl: "https://n5w9f9eo8b.execute-api.us-east-2.amazonaws.com",
+        stage: "prod",
+        invokeUrl: 'https://n5w9f9eo8b.execute-api.us-east-2.amazonaws.com/prod', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
